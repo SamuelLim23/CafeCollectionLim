3b1838722bbfcc2e17d61b0cccb7efe6971ddb8e
Samuel Lim <samuel.lim23@d155.org>
2022-09-12 08:46:15 -0500

    Initial Commit

---
+++ CafeCollectionLim/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CafeCollectionLim
+//
+//  Created by SAMUEL LIM on 9/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
